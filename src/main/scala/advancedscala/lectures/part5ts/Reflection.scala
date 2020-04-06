22f9bc58227c38cc0889910b2702103bfc5030ab
Sriram <srirambtecit@gmail.com>
2020-04-06 08:28:51 +0100

    TypeTag reflection

--- src/main/scala/advancedscala/lectures/part5ts/Reflection.scala
+++ src/main/scala/advancedscala/lectures/part5ts/Reflection.scala
@@ -37,4 +37,55 @@ object Reflection extends App {
   val method = reflected.reflectMethod(methodSymbol)
   // 5 - invoke
   method.apply()
+
+  // type erasure
+
+  // pain point #1
+  val numbers = List(1, 2, 3)
+  numbers match {
+    case listOfStrings: List[String] => println("list of strings")
+    case listOfInts: List[Int] => println("list of ints")
+  }
+
+  // pain point #2
+  // def processList(list: List[String]): Int = 42
+  // def processList(list: List[Int]): Int = 41
+
+  // TypeTags
+  // 0 - import
+  import ru._
+
+  // 1 - creating manually
+  val ttag = typeTag[Person]
+  println(ttag.tpe)
+
+  class MyMap[K, V]
+
+  // 2 - pass type tags as implicit parameter
+  def getTypeArguments[T](value: T)(implicit typeTag: TypeTag[T]) = typeTag.tpe match {
+    case TypeRef(_, _, typeArguments) => typeArguments
+    case _ => List()
+  }
+
+  val myMap = new MyMap[String, Int]
+  val typeArgs = getTypeArguments(myMap)
+  println(typeArgs)
+
+  def isSubtype[A, B](implicit ttagA: TypeTag[A], ttagB: TypeTag[B]): Boolean = {
+    ttagA.tpe <:< ttagB.tpe
+  }
+
+  class Animal
+  class Cat extends Animal
+  println(isSubtype[Cat, Animal])
+
+  // and I have an instance
+
+  // 3 - method symbol
+  val anotherMethodSymbol = typeTag[Person].tpe.decl(ru.TermName(methodName)).asMethod
+  // 4 - reflect the method
+  val sameMethod = reflected.reflectMethod(anotherMethodSymbol)
+  // 5 - invoke method
+  sameMethod.apply()
+
 }
